de1bb864f9b7b7896a8e71b6a4e7a0fe7070658b
Mai Houa Vue <mhvue4@gmail.com>
2020-04-28 23:25:23 -0500

    add css of images to grid instead which helped with layout of images

--- src/components/ImgCard/index.js
+++ src/components/ImgCard/index.js
@@ -3,17 +3,12 @@ import "./style.css";
 
 function ImgCard(props){
     return(
-            <div className=".cardHolder container-fluid">
                 <img className="img-responsive"
                 id={props.id}
                 alt={props.name} 
                 src={props.image} 
                 onClick={() => props.clickedMethod(props.id)}
                 />
-            
-            </div>
-
-
     );
 }
 
